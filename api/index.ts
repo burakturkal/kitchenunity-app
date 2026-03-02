b703c3dd8bcad23c5542010bcf5b88b14fabc63c
Burak <bturkal.eee@gmail.com>
2026-03-02 02:01:25 -0600

    Save local changes before push

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import serverless from 'serverless-http';
-import app from '../server';
-
-const handler = serverless(app as any);
-
-export default async function (req: any, res: any) {
-  return handler(req, res);
-}
